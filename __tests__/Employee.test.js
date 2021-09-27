c8d7aa5ff8f1d72fed68430fcfa793b51edf832a
Asa Grace <asag182@gmail.com>
2021-09-26 23:10:46 -0600

    employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,27 @@ test('creates an Employee object', () => {
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe('john@gmail.com');
 });
+
+test('create name for the employee', () => {
+    const name = "John";
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+});
+
+test('create id for employee', () => {
+    const id = "1234";
+    const employee = new Employee(id);
+    expect(employee.id).toBe(id);
+});
+
+test('create email for employee', () => {
+    const email = "test@test.com";
+    const employee = new Employee(email);
+    expect(employee.email).toBe(email);
+});
+
+test('create role for employee', () => {
+    const role = "employee";
+    const employee = new Employee(role);
+    expect(employee.role).toBe(role);
+});
--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-function Employee(name) {
-    
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const Employee = require('./lib/Employee');
+
+function employee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the employee?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Employee name Required!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employees ID number?',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Employee Id Required!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employees Email Address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Employee Email address Required!');
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'select a role for your employee',
+            choices: ['Manager', 'Engineer', 'Employee']
+        }
+    ])
 }
+
+employee();
